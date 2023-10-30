58d562cb2cae114901c4718f71a0f102fd0c33ad
carlosandre-dev <andre.tjlba@gmail.com>
2023-10-30 18:34:30 -0300

    Entendo Arrays

---
+++ colecoes/array.rb
@@ -0,0 +1,7 @@
+browsers = ['Chrome', 'Firefox', 'Safari', 'Opera', 'IE']
+browsers.push('Edge')
+puts browsers
+
+puts ("O array possue #{browsers.length} elementos")
+puts ("O primeiro elemento do array é #{browsers.first}")
+puts ("O último elemento do array é #{browsers.last}")
